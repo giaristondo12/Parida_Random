a4c06701cc9c551bf737c0520b07508c416d6d00
Magí Aristondo Alzina <magiaristondo@paucasesnovescifp.cat>
2024-04-16 03:04:25 +0200

    Actualització gran a GitHub!!! Afegit menu inici i menú dificultats. Nova estètica i funcionalitat.

--- ParidaRandom.js
+++ ParidaRandom.js
@@ -1,6 +1,6 @@
-let num;
+let num; // número aleatori a endevinar
 let message = document.querySelector("missatge");
-let color = message.style.backgroundColor;
+let color = "#93b247";
 let endevinat;
 let intents = document.querySelector("intents");
 let numeroIntroduit = document.querySelector("numero_introduit");
@@ -10,16 +10,25 @@ let menu = document.getElementById("menu");
 let zonaJoc = document.getElementById("zonaJoc");
 let menuDif = document.getElementById("menuDificultats");
 
+let totalCaselles;
 
 let numintents = 0;
 
-const inici = () =>
+const botoDificultat = (files, columnes) =>
 {
-    nouNum(100);
-    generarTaulell(100);
+    let texte = document.querySelector("texte");
+
+    totalCaselles = files * columnes;
+    nouNum(totalCaselles);
+    generarTaulell(totalCaselles);
+    goZonaJoc();
+    setCSS(files, columnes);
+
+    texte.textContent = "Introdueix un número entre 0 i " + totalCaselles + ":";
 }
 
-const generarTaulell = (caselles) => {
+const generarTaulell = (caselles) =>
+{
     let marcador = document.querySelector("#marcador");
     marcador.innerHTML = "";
     for (let cas = 1; cas <= caselles; cas++) {
@@ -30,12 +39,11 @@ const generarTaulell = (caselles) => {
         marcador.appendChild(casella);
         casella.addEventListener("click", (event) => { action(cas) });
     }
-    marcats = document.querySelectorAll("#marcador > div");
 }
 
-const nouNum = (max) =>
+const nouNum = (total) =>
 {
-    num = Math.floor(Math.random() * max + 1);
+    num = Math.floor(Math.random() * total + 1);
 }
 
 const actualitzarInfo = (input) =>
@@ -58,9 +66,9 @@ const comprovarNum = (introduit) =>
     canviarColor("#ff9700");
     switch (true)
     {
-        case input < 1 || input > 100:
+        case input < 1 || input > totalCaselles:
         {
-            numeroIntroduit.textContent = "Ha de ser entre 0 i 100!";
+            numeroIntroduit.textContent = "Ha de ser entre 0 i " + totalCaselles + "!";
             numeroIntroduit.style.backgroundColor = "#ee4141";
             numeroIntroduit.style.color = "#f5d0ac";
             correcte = false;
@@ -99,10 +107,20 @@ const comprovarNum = (introduit) =>
 
         if (!endevinat)
         {
-            setTimeout(function() { canviarColor(color) }, 100);
             let colorMajorMenor;
-            if (input < num) colorMajorMenor = "#ffe100";
-            else colorMajorMenor = "#00d0ff";
+            let colorMessage;
+            if (input < num)
+            {
+                colorMajorMenor = "#ffe100";
+                colorMessage = "#dcc411";
+            }
+            else
+            {
+                colorMajorMenor = "#00d0ff";
+                colorMessage = "#28b2d0";
+            }
+            canviarColor(colorMessage);
+            setTimeout(function() { canviarColor(color) }, 750);
             document.querySelector(".marcats:nth-of-type(" + (input) + ")").style.backgroundColor = colorMajorMenor;
         }
         else
@@ -136,6 +154,7 @@ const goMenuDif = () =>
     menuDif.style.display = "flex";
     menu.style.display = "none";
     zonaJoc.style.display = "none";
+    amagarInput();
 }
 
 const goMenu = () =>
@@ -146,11 +165,21 @@ const goMenu = () =>
     reset();
 }
 
+const setCSS = (files, columnes) =>
+{
+    let marcador = document.getElementById("marcador");
+
+    marcador.style.gridTemplateColumns = "repeat(" + columnes + ", 30px)";
+    marcador.style.gridTemplateRows = "repeat(" + files + ", 30px)";
+    marcador.style.width = columnes / 10 * 300 + "px";
+    marcador.style.height = files / 10 * 300 + "px";
+}
+
 const action = (a) =>
 {
     if (endevinat)
     {
-        nouNum(100);
+        nouNum(totalCaselles);
         reset();
         document.querySelector("input[type='number']").focus();
     }
@@ -169,8 +198,16 @@ const action = (a) =>
     }
 }
 
+const reiniciar = () =>
+{
+    reset();
+    nouNum(totalCaselles);
+}
+
 const reset = () =>
 {
+    let marcats = document.querySelectorAll(".marcats");
+
     numintents = 0;
     numeroIntroduit.textContent = "No has intentat cap número";
     intents.textContent = "Duus: " + numintents + " intents";
@@ -191,4 +228,40 @@ window.onkeydown = (event) => {
             action();
             break;
     }
+}
+
+const seleccionarDif = () =>
+{
+    let files = document.getElementById("inputFiles").value;
+    let columnes = document.getElementById("inputColumnes").value;
+
+    document.getElementById("inputFiles").value = "";
+    document.getElementById("inputColumnes").value = "";
+
+    files = Math.round(files);
+    columnes = Math.round(columnes);
+
+    if (files !== "" && columnes !== "" && files >= 5 && columnes >= 5 && files <= 35 && columnes <= 35)
+        botoDificultat(files, columnes);
+    else
+    {
+        let titol = document.querySelector("#contingutDificultats > titol");
+        titol.textContent = "Entre 5 i 35";
+        setTimeout(function() { titol.textContent = "Tria la\ndificultat" }, 2000);
+    }
+}
+
+let botons = document.getElementById("botonsDif");
+let inputs = document.getElementById("inputsDif");
+
+const mostrarInput = () =>
+{
+    botons.style.display = "none";
+    inputs.style.display = "unset";
+}
+
+const amagarInput = () =>
+{
+    botons.style.display = "unset";
+    inputs.style.display = "none";
 }
